25df55a211a11f158361a2e18fa9b7c9eee8894e
Thomas Tarpin-Lyonnet <thomas.tarpin@gmail.com>
2022-11-24 10:13:05 +0800

    Added graphics.py

--- life.py
+++ life.py
@@ -1,6 +1,13 @@
 from random import *
 from copy import *
+from graphics import *
 import time
+
+cell_size = 20
+population = 15
+grid_width = cell_size * population
+
+win = GraphWin("Game Of Life", grid_width, grid_width)
 
 def print_matrix(matrix, matrix_size):
     for row in range(matrix_size):
@@ -13,18 +20,38 @@ def print_matrix(matrix, matrix_size):
         print(line)
     print()
 
-def main():
-    # Population => number of cells in the matrix
-    population = 20
+def draw_cell(x, y, alive):
+    cell = Rectangle(Point(x, y), Point(x+cell_size, y+cell_size))
+    cell.setOutline(color_rgb(255, 255, 255))
+    if alive:
+        cell.setFill(color_rgb(0, 0, 0))
+    else:
+        cell.setFill(color_rgb(255, 255, 255))
+    cell.draw(win)
 
-    # Create and initialize the matrix containing the cell states
-    matrix = [[2 for i in range(population)] for j in range(population)]
+def draw_grid(grid):
     for row in range(population):
         for col in range(population):
-            matrix[row][col] = randint(a=0, b=1)
+            x = row * cell_size
+            y = col * cell_size
+            alive = False
+            if grid[row][col] == 1:
+                alive = True
+            draw_cell(x, y, alive)   
+
+def main():
+    # Population => number of cells in the matrix
+    # population = 20
+
+    # Create and initialize the matrix containing the cell states
+    grid = [[2 for i in range(population)] for j in range(population)]
+    for row in range(population):
+        for col in range(population):
+            grid[row][col] = randint(a=0, b=1)
 
     # Display the generation #1
-    print_matrix(matrix, population)
+    # print_matrix(grid, population)
+    draw_grid(grid)
 
     # Game of life rules
     # 1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.
@@ -34,32 +61,36 @@ def main():
 
     gen_number = 5000
     for gen in range(gen_number):
-        old_matrix = deepcopy(matrix)
+        old_grid = deepcopy(grid)
         for row in range(1, population-1):
             for col in range(1, population-1):
                 alive_neighbours = 0
-                # Count the living neighbours around old_matrix[row][col]
+                # Count the living neighbours around old_grid[row][col]
                 for row_eval in range(row-1, row+2):
                     for col_eval in range(col-1, col+2):
-                        if old_matrix[row_eval][col_eval] == 1:
+                        if old_grid[row_eval][col_eval] == 1:
                             alive_neighbours += 1
-                # Don't count the reference cell (e.g. old_matrix[row][col]) if living
-                if old_matrix[row][col] == 1:
+                # Don't count the reference cell (e.g. old_grid[row][col]) if living
+                if old_grid[row][col] == 1:
                     alive_neighbours -= 1
 
                 # Check game of life rules
-                if old_matrix[row][col] == 1 and (alive_neighbours < 2 or alive_neighbours > 3):
-                    matrix[row][col] = 0
-                elif old_matrix[row][col] == 1 and (alive_neighbours == 2 or alive_neighbours == 3):
-                    matrix[row][col] = old_matrix[row][col]
-                elif old_matrix[row][col] == 0 and alive_neighbours == 3:
-                    matrix[row][col] = 1
+                if old_grid[row][col] == 1 and (alive_neighbours < 2 or alive_neighbours > 3):
+                    grid[row][col] = 0
+                elif old_grid[row][col] == 1 and (alive_neighbours == 2 or alive_neighbours == 3):
+                    grid[row][col] = old_grid[row][col]
+                elif old_grid[row][col] == 0 and alive_neighbours == 3:
+                    grid[row][col] = 1
                 else:
-                    matrix[row][col] = old_matrix[row][col]
+                    grid[row][col] = old_grid[row][col]
         
         # Display the new generation
-        print_matrix(matrix, population)
+        # print_matrix(grid, population)
+        draw_grid(grid)
         # Slow down the process
         # time.sleep(0.1)
+
+    win.getMouse()
+    win.close()
 
 main()
