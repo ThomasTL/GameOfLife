b32314b59ba8aac96a6c01687da28139f8d7a67d
Thomas Tarpin-Lyonnet <thomas.tarpin@gmail.com>
2022-11-24 23:21:17 +0800

    Working version

--- life.py
+++ life.py
@@ -3,22 +3,12 @@ from copy import *
 from graphics import *
 import time
 
-cell_size = 20
-population = 15
-grid_width = cell_size * population
+cell_size = 15
+columns = 25
+grid_width = cell_size * columns
 
-win = GraphWin("Game Of Life", grid_width, grid_width)
-
-def print_matrix(matrix, matrix_size):
-    for row in range(matrix_size):
-        line = ""
-        for col in range(matrix_size):
-            cell = " "
-            if matrix[row][col] == 1:
-                cell = "@"
-            line += cell + " "
-        print(line)
-    print()
+win = GraphWin("Game Of Life", grid_width, grid_width, autoflush=False)
+win.setBackground("white")
 
 def draw_cell(x, y, alive):
     cell = Rectangle(Point(x, y), Point(x+cell_size, y+cell_size))
@@ -29,41 +19,75 @@ def draw_cell(x, y, alive):
         cell.setFill(color_rgb(255, 255, 255))
     cell.draw(win)
 
-def draw_grid(grid):
-    for row in range(population):
-        for col in range(population):
+def draw_grid(grid, old_grid):
+    for row in range(columns):
+        for col in range(columns):
             x = row * cell_size
             y = col * cell_size
             alive = False
-            if grid[row][col] == 1:
+            draw = False
+            # Only draw if the cell changes state
+            if grid[row][col] == 1 and old_grid[row][col] == 0:
                 alive = True
-            draw_cell(x, y, alive)   
+                draw = True
+            elif grid[row][col] == 0 and old_grid[row][col] == 1:
+                draw = True
 
+            if draw == True: 
+                draw_cell(x, y, alive) 
+
+def shapes(grid, shape):
+    if shape == "SHAPE_1":
+        grid[10][15] = 1
+        grid[11][14] = 1
+        grid[11][15] = 1
+        grid[11][16] = 1
+        grid[12][15] = 1
+    elif shape == "SHAPE_2":
+        grid[5][5] = 1
+        grid[6][5] = 1
+        grid[7][5] = 1
+    elif shape == "SHAPE_3":
+        grid[5][25] = 1
+        grid[6][25] = 1
+        grid[7][25] = 1
+        grid[7][26] = 1
+        grid[6][27] = 1
+    elif shape == "SHAPE_4":
+        grid[20][15] = 1
+        grid[21][15] = 1
+        grid[22][15] = 1
+        grid[22][16] = 1
+        grid[22][17] = 1
+        grid[21][17] = 1
+        grid[23][17] = 1
+    
 def main():
-    # Population => number of cells in the matrix
-    # population = 20
-
     # Create and initialize the matrix containing the cell states
-    grid = [[2 for i in range(population)] for j in range(population)]
-    for row in range(population):
-        for col in range(population):
-            grid[row][col] = randint(a=0, b=1)
+    grid = [[0 for i in range(columns)] for j in range(columns)]
+    old_grid = [[0 for i in range(columns)] for j in range(columns)]
 
-    # Display the generation #1
-    # print_matrix(grid, population)
-    draw_grid(grid)
+    defined_shapes = False
+    if defined_shapes != True:
+        # Random initialization of the grid
+        for row in range(columns):
+            for col in range(columns):
+                if row != 0 and row != columns-1 and col != 0 and col != columns-1:
+                    grid[row][col] = randint(a=0, b=1)
+    else:
+        shapes(grid, "SHAPE_1")
 
-    # Game of life rules
-    # 1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-    # 2. Any live cell with two or three live neighbours lives on to the next generation.
-    # 3. Any live cell with more than three live neighbours dies, as if by overpopulation.
-    # 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+    # Draw the generation #1
+    draw_grid(grid, old_grid)
+    update(30)
 
+    # Running Game of life rules
+    # https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
     gen_number = 5000
     for gen in range(gen_number):
         old_grid = deepcopy(grid)
-        for row in range(1, population-1):
-            for col in range(1, population-1):
+        for row in range(1, columns-1):
+            for col in range(1, columns-1):
                 alive_neighbours = 0
                 # Count the living neighbours around old_grid[row][col]
                 for row_eval in range(row-1, row+2):
@@ -85,9 +109,9 @@ def main():
                     grid[row][col] = old_grid[row][col]
         
         # Display the new generation
-        # print_matrix(grid, population)
-        draw_grid(grid)
-        # Slow down the process
+        draw_grid(grid, old_grid)
+        update(30)
+        # Temporisation
         time.sleep(0.1)
 
     win.getMouse()
